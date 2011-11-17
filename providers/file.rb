91ae95410d6b330708b90b0dd56192e1310f888f
Juanje Ojeda <jojeda@emergya.es>
2011-11-17 01:51:03 +0100

    Removed unused attributes

--- providers/file.rb
+++ providers/file.rb
@@ -1,9 +1,5 @@
 action :replace do
   file new_resource.name do
     replace(new_resource.before, new_resource.after) if include?(new_resource.before)
-    owner new_resource.owner
-    group new_resource.group
-    mode "0644"
-    action :touch
   end
 end
