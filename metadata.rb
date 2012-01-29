b1f9c18fec9e45f5f955ce728f9e469d91b4aabb
Juanje Ojeda <juanje.ojeda@gmail.com>
2012-01-29 22:26:29 +0000

    Updated some metadata (license, version, email and description)

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "Juanje Ojeda"
-maintainer_email "jojeda@emergya.com"
-license          "GPL v2"
-description      "Manage configurations"
+maintainer_email "juanje.ojeda@gmail.com"
+license          "Apache 2.0"
+description      "LWRP to manage configuration files"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.1.0"
